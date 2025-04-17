aed7bdd1501d56ca50d3c8ec2bc1a7b11bb0251e
남기원 <aa8791@naver.com>
2025-04-17 10:23:11 +0900

    CTRLLOLER

---
+++ _proj_spring_ktpn/src/main/java/kr/or/ktpn/controller/Ctrl_mb_1000mt.java
@@ -0,0 +1,16 @@
+package kr.or.ktpn.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class Ctrl_mb_1000mt {
+
+	@RequestMapping(value = "/member", method = RequestMethod.GET)
+	public String listMember() {
+		
+		return "login";
+	}
+	
+}
