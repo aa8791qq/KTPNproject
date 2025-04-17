779e365504aeda59c3923c0faf5fa5a858219838
남기원 <aa8791@naver.com>
2025-04-17 11:26:37 +0900

    DAO(impl)

--- _proj_spring_ktpn/src/main/java/kr/or/ktpn/dao/KW_DAO_mb_1000impl.java
+++ _proj_spring_ktpn/src/main/java/kr/or/ktpn/dao/KW_DAO_mb_1000impl.java
@@ -2,6 +2,8 @@ package kr.or.ktpn.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import kr.or.ktpn.dto.KW_DTO_MB_1000MT;
@@ -9,6 +11,9 @@ import kr.or.ktpn.dto.KW_DTO_MB_1000MT;
 @Repository
 public class KW_DAO_mb_1000impl implements KW_DAO_mb_1000mt {
 
+	@Autowired
+	SqlSession sqls;
+	
 	@Override
 	public List<KW_DTO_MB_1000MT> selectMember() {
 		// TODO Auto-generated method stub
